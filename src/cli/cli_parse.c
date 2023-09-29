6c5b90702e076302f8bffd0c22510175a81810df
Letícia Aguiar <letsaguiar@yandex.com>
2023-09-29 11:46:10 -0300

    refactor: cli parse

--- src/cli/cli_parse.c
+++ src/cli/cli_parse.c
@@ -7,14 +7,12 @@ t_cli   *cli_parse(char **args)
     t_cli   *cli;
 
     cli = (t_cli *) malloc(sizeof (t_cli));
-    for (int i = 1; args[i]; i++)
+    cli->operation = args[1];
+    cli->package = args[2];
+    for (int i = 3; args[i]; i++)
     {
         if (strcmp(args[i], "--version") == 0)
-            cli->version = args[i];
-        else if (!cli->operation)
-            cli->operation = args[i];
-        else if (!cli->package)
-            cli->package = args[i];
+            cli->version = args[++i];
     }
     return (cli);
 }
