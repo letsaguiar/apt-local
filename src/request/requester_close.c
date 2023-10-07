39e90cc6abd91c1c743c2bc85ce7479f06d634e5
Letícia Aguiar <letsaguiar@yandex.com>
2023-10-07 06:13:14 -0300

    feat: Init python project

--- src/request/requester_close.c
+++
@@ -1,6 +0,0 @@
-#include <curl/curl.h>
-
-void    requester_close()
-{
-    curl_global_cleanup();
-}
--- src/request/requester_init.c
+++
@@ -1,6 +0,0 @@
-#include <curl/curl.h>
-
-void    requester_init()
-{
-    curl_global_init(CURL_GLOBAL_ALL);
-}
