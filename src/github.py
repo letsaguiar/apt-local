553adc69a8b00aa2f7892e212bf885dc628a8f3d
Letícia Aguiar <letsaguiar@yandex.com>
2023-10-10 16:02:15 -0300

    feat(install): add support for release selection
    #3

--- src/github.py
+++ src/github.py
@@ -4,27 +4,35 @@ import requests
 
 from typing import Dict
 
-def get_package_releases(github_url: str) -> Dict:
+def get_package_release_all(github_url: str) -> Dict:
     try:
-        package_releases = requests.get(f"{github_url}/releases").text 
+        package_releases = requests.get(f"{github_url}/releases").text
         return (json.loads(package_releases))
     except:
         click.echo("Unable to fetch Github API")
         exit()
 
-def get_package_latest_release(github_url) -> Dict:
+def get_package_release_latest(github_url: str) -> Dict:
     try:
-        package_releases = requests.get(f"{github_url}/releases").text
-        package_releases = json.loads(package_releases)
-
+        package_releases = get_package_release_all(github_url)
         return (package_releases[0])
     except:
         click.echo("Unable to fetch Github API")
         exit()
 
+def get_package_release(github_url: str, release: str) -> Dict:
+    package_releases = get_package_release_all(github_url)
+
+    for package_release in package_releases:
+        if package_release["tag_name"] == release:
+            return (package_release)
+
+    click.echo("Unable to find the selected release")
+    exit()
+
 def get_package_asset(package_release: Dict, asset_name: str) -> Dict:
     for asset in package_release["assets"]:
-        if asset["name"] == asset_name:
+        if asset["name"] == asset_name.format(version=package_release["tag_name"]):
             return (asset)
 
     click.echo("Unable to find the selected asset")
--- src/main.py
+++ src/main.py
@@ -12,21 +12,27 @@ def cli():
 @click.argument('package_name')
 def list(package_name: str):
     package_config = config.get_package_config(package_name)
-    package_releases = github.get_package_releases(package_config["github_url"])
+    package_releases = github.get_package_release_all(package_config["github_url"])
 
     for release in package_releases:
         click.echo(release["tag_name"])
 
 @click.command()
 @click.argument('package_name')
-def install(package_name: str):
-	package_config = config.get_package_config(package_name)
-	package_release = github.get_package_latest_release(package_config['github_url'])
-	package_asset = github.get_package_asset(package_release, package_config["assets"]["linux"])
+@click.option('--release', '-r', help='github tag_name of the desired release')
+def install(package_name: str, release: str):
+    package_config = config.get_package_config(package_name)
 
-	installer.download_and_extract(package_config, package_asset)
-	installer.build_local_path()
-	installer.clean_installation()
+    if release:
+        package_release = github.get_package_release(package_config['github_url'], release)
+    else:
+        package_release = github.get_package_release_latest(package_config['github_url'])
+
+    package_asset = github.get_package_asset(package_release, package_config["assets"]["linux"])
+
+    installer.download_and_extract(package_config, package_asset)
+    installer.build_local_path()
+    installer.clean_installation()
 
 cli.add_command(list)
 cli.add_command(install)
